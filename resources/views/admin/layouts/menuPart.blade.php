fa8f3e3888a45682d8c488b417c782b7c48bd912
zz <527844046@qq.com>
2017-08-09 21:21:57 +0800

    测试

--- resources/views/admin/layouts/menuPart.blade.php
+++ resources/views/admin/layouts/menuPart.blade.php
@@ -1,6 +1,6 @@
 @foreach($menus as $key=>$menu)
 @if(Auth::guard('admin')->user() && Auth::guard('admin')->user()->visible($menu->roles))
-<li class="nav-item {{is_active($menu->id)}}">
+<li class="nav-item">
     <a href="@if($menu->hasChildren())javascript:;@else{{url(config('admin.prefix').'/'.$menu->uri)}}@endif" class="nav-link @if($menu->hasChildren())nav-toggle @endif">
         <i class="fa {{$menu->icon}}"></i>
         <span class="title">{{$menu->title}}</span>
