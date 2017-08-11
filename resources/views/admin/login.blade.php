4b35887bc81a3d314f775c162a7fdfb0f8e5e3f7
zzDylan <527844046@qq.com>
2017-08-11 11:38:53 +0800

    title

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -8,7 +8,7 @@
 
     <head>
         <meta charset="utf-8" />
-        <title>Metronic Admin Theme #4 | User Login 4</title>
+        <title>后台管理系统登录</title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta content="Preview page of Metronic Admin Theme #4 for " name="description" />
